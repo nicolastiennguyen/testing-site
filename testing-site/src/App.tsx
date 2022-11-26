1c0f6f1d7b482c7922545fa4a4c74dbb37660170
Nick <nicolastiennguyen@gmail.com>
2022-11-26 15:31:50 -0800

    added counter

--- testing-site/src/App.tsx
+++ testing-site/src/App.tsx
@@ -2,14 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { MantineProvider } from '@mantine/core';
+import { Group, Button, Text } from '@mantine/core';
+import { useCounter } from '@mantine/hooks';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, handlers] = useCounter(0, { min: 0, max: 10 });
 
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <div className="App">
-
+        <Text>Count: {count}</Text>
+        <Group position="center">
+          <Button onClick={handlers.increment}>Increment</Button>
+          <Button onClick={handlers.decrement}>Decrement</Button>
+          <Button onClick={handlers.reset}>Reset</Button>
+          <Button onClick={() => handlers.set(5)}>Set 5</Button>
+        </Group>
       </div>
     </MantineProvider>
   )
